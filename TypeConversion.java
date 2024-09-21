6168e5c7821d431ca067dbe1d67a8a67f6dd562f
praveenkumar <praveenkumarnaidu88@gmail.com>
2024-09-21 21:56:18 +0530

    DAY 1 Programming in java

---
+++ TypeConversion.java
@@ -0,0 +1,30 @@
+public class TypeConversion {
+	public static void main(String[] args) {
+		// byte b=127;
+		// int a=b;
+		// System.out.println(b);
+		// System.out.println(a);
+		
+		// byte b1=125;
+		// int a0=b1;
+		// System.out.println(b1);
+		// System.out.println(a0);
+		
+		// int aa=257;
+		// byte k=(byte)aa;
+		
+		// float f=5.6f;
+		// int t=(int)f;
+        // System.out.println(t);	
+
+		// int a=2567;
+		// byte b=(byte)a;
+		// System.out.println(b);
+		
+		byte a=10;
+		byte b=20;
+		int t=a*b;
+		System.out.println(t);
+	
+	}
+}
