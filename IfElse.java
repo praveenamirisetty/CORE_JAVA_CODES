82986c0c5d5bc7c4bf09aed173b7fb3463261b3a
praveenkumar <praveenkumarnaidu88@gmail.com>
2024-09-22 12:04:14 +0530

    DAY 2 Programming in Java

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+	public static void main(String[] args) {
+		int x=8;
+		int y=7;
+		int z=8;
+		
+		if(x>y && x>z) {
+			System.out.println(x);
+		}else{
+			System.out.println(y);
+        }
+		
+	}
+}
